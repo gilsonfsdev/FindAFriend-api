f79bed736bbe58e8a1667870bdc5bace3671bbb5
Gilson Ferreira <gilsonfsdev@gmail.com>
2023-10-05 14:47:33 -0300

    Update register-pet.spec.ts

--- src/use-cases/register-pet.spec.ts
+++ src/use-cases/register-pet.spec.ts
@@ -1,18 +1,33 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
 import { RegisterPetUseCase } from './register-pet'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
 
+let orgsRepository: InMemoryOrgsRepository
 let petsRepository: InMemoryPetsRepository
 let sut: RegisterPetUseCase
 
 describe('Register Pet Use Case', () => {
   beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
     petsRepository = new InMemoryPetsRepository()
     sut = new RegisterPetUseCase(petsRepository)
   })
 
   it('should be able to register a pet', async () => {
-    const pet = await petsRepository.create({
+    const org = await orgsRepository.create({
+      title: 'Org 01',
+      president: 'Gilson Ferreira',
+      password: '1234567',
+      email: 'teste@teste.com',
+      whatsapp: '44998505620',
+      adress: 'rua chile 19',
+      cep: '87050120',
+    })
+
+    const orgId = org.id
+
+    const { pet } = await sut.execute({
       name: 'Teste 01',
       description: 'Teste description',
       type: 'CAT',
@@ -22,7 +37,7 @@ describe('Register Pet Use Case', () => {
       independency: 'LOW',
       city: 'Cabedelo',
       photo: 'foto 1',
-      org_id: 'org-1',
+      orgId,
     })
 
     expect(pet.id).toEqual(expect.any(String))
